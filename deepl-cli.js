7bcdbb8b9fc4f2f9d946a492652cd7de3a728704
milahu <milahu@gmail.com>
2021-05-23 23:00:26 +0200

    use cache folder in $HOME

--- deepl-cli.js
+++ deepl-cli.js
@@ -3,8 +3,9 @@
 const jsdom = require('jsdom');
 const fs = require('fs');
 const path = require('path');
+const { default: getPath } = require('platform-folders');
 
-const cacheDir = 'cache';
+const cacheDir = getPath('cache') + '/deepl-client-jsdom';
 const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36";
 const targetTextSelector = '#target-dummydiv';
 
